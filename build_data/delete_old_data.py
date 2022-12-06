5e1fc2d4eb6489b07f3ed81c67ab79cc03215f54
khengyun <khgammingcraft@gmail.com>
2022-12-07 05:31:25 +0700

    update delete file

--- build_data/delete_old_data.py
+++ build_data/delete_old_data.py
@@ -1,9 +1,8 @@
-
+#   THINK BEFORE RUN THIS FUCKING FILE __ GOODLUCK __
 
 import os, shutil
 # path to your image folder
 folder_image = '/home/fptlap/Documents/build_data/data/images'
-os.remove("/home/fptlap/Documents/build_data/data/log.csv")
 for filename in os.listdir(folder_image):
     file_path = os.path.join(folder_image, filename)
     try:
@@ -14,4 +13,4 @@ for filename in os.listdir(folder_image):
     except Exception as e:
         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
-open('data/log.csv', 'a')
+open('data/log.csv', 'w')
